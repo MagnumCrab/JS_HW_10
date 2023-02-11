1b95e006ee55791854ebc12740079ef08b06fafa
MagnumCrab <magnumkrab@gmail.com>
2023-02-11 22:02:59 +0300

    .\data.js

---
+++ data.js
@@ -0,0 +1,32 @@
+const dataInfo = `[
+        {
+            "type": "general",
+            "setup": "What do you call a bee that can't make up its mind?",
+            "punchline": "A maybe.",
+            "id": "379"
+        },
+        {
+            "type": "general",
+            "setup": "What do you call a cow with two legs?",
+            "punchline": "Lean beef.",
+            "id": "199"
+        },
+        {
+            "type": "general",
+            "setup": "Want to hear a chimney joke?",
+            "punchline": "Got stacks of em! First one's on the house",
+            "id": "152"
+        },
+        {
+            "type": "general",
+            "setup": "What is the tallest building in the world?",
+            "punchline": "The library, its got the most stories!",
+            "id": "253"
+        },
+        {
+            "type": "general",
+            "setup": "Which side of the chicken has more feathers?",
+            "punchline": "The outside.",
+            "id": "300"
+        }
+]`
